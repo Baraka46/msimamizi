29a6c611f58de199812eab747de03b3d58a5281b
munisi <munisib2@gmail.com>
2024-12-22 01:28:21 +0300

    error migration

---
+++ app/Http/Controllers/GeneralExpenseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GeneralExpense;
+use Illuminate\Http\Request;
+
+class GeneralExpenseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GeneralExpense $generalExpense)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GeneralExpense $generalExpense)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, GeneralExpense $generalExpense)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GeneralExpense $generalExpense)
+    {
+        //
+    }
+}
--- database/migrations/2024_12_21_214239_create_users_table.php
+++ database/migrations/2024_12_21_214239_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable(); 
             $table->string('password');
+            $table->enum('role', ['owner', 'admin', 'supervisor']);
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
