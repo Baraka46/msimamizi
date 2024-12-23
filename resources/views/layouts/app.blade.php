cbfd246b9b79e9467e0030a26eace8348e283594
munisi <munisib2@gmail.com>
2024-12-24 02:49:41 +0300

    auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -18,23 +20,24 @@
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
+
+    
         <x-banner />
 
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
+           
+            <div class="flex">
+                <!-- Sidebar with background color -->
+                
+                    @include('components.sidebar')
+               
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                {{ $slot }} 
+
             </main>
         </div>
 
