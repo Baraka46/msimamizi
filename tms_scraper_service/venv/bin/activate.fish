0e52ed921724197836001f86208f1ebf4261801d
munisi <munisib2@gmail.com>
2025-04-21 22:57:41 +0300

    python fix

--- tms_scraper_service/venv/bin/activate.fish
+++ tms_scraper_service/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/munisi/Munisi/tms_scraper_service/venv
+set -gx VIRTUAL_ENV /home/munisi/Documents/laravel/msimamizi/tms_scraper_service/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
